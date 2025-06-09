5ceac8ef856137aaaf2b85e9cbefd84957fadcdb
leebonggu <leebonggu12@hanmail.net>
2025-06-10 01:15:12 +0900

    Init

---
+++ src/test/java/com/bonggu/study/springcoreprinciple/SpringCorePrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonggu.study.springcoreprinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePrincipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
